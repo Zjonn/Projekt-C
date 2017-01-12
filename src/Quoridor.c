b8a6ce5522e5d761ba6046dd12ad235db9c476af
Zjonn <291111@uwr.edu.pl>
2017-01-12 15:30:15 +0100

    Stworzyłem projekt

---
+++ src/Quoridor.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Quoridor.c
+ Author      : Daniel Dubiel
+ Version     : 1.0
+ Copyright   : PizzaWare licence
+ Description : Gra Quoridor w C
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts(""); /* prints  */
+	return EXIT_SUCCESS;
+}
